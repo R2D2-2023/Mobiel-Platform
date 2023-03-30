c9ca8463770474043b7e55f12d52034347590263
jurgen1507 <jbraan01@gmail.com>
2023-03-30 15:27:43 +0200

    gyro toegevoegd, motoren passen aan op hoek. Moet nog gesmooth worden

--- Arduino/combiCode.ino
+++ Arduino/combiCode.ino
@@ -2,7 +2,9 @@
 #include <L298N.h>
 
 #define RPLIDAR_MOTOR 10 
-
+#include "Wire.h"
+#include <MPU6050_light.h>
+MPU6050 mpu(Wire);
 
 // motor pin definition
 #define M_EN 9
@@ -25,7 +27,10 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   lidar.begin(Serial);
   pinMode(RPLIDAR_MOTOR, OUTPUT);
-  
+   Wire.begin();
+ byte status = mpu.begin();
+   delay(1000);
+   mpu.calcOffsets(); // gyro and accelero
 }
 
 void loop() {
@@ -43,10 +48,8 @@ void loop() {
           motor1.forward();
           motor2.forward();
          }else {
-          motor1.setSpeed(255); // turn the LED on (HIGH is the voltage level)
-          motor2.setSpeed(255);
-          motor1.forward();
-          motor2.forward();
+          rotate(0, motor1, motor2);
+
        }
        } 
        minDistance = 100000;
@@ -68,5 +71,31 @@ void loop() {
        analogWrite(RPLIDAR_MOTOR, 255);
     }
   }
+}
+
+void rotate(int angle, L298N motor1, L298N motor2){
+  mpu.update();
+
+  if (mpu.getAngleZ() > angle + 0.02 ){
+      motor1.setSpeed(255);
+      motor2.setSpeed(0);
+      motor1.forward();
+      motor2.forward(); 
+    }
+  else if (mpu.getAngleZ() < angle - 0.02){\
+      motor1.setSpeed(0);
+      motor2.setSpeed(255);
+
+      motor1.forward();
+      motor2.forward(); 
+    } else{
+      motor1.setSpeed(255);
+      motor2.setSpeed(255);
+
+      motor1.forward();
+      motor2.forward(); 
+    }
+
+ 
 }
 
