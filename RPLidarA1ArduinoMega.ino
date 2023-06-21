6df031a0c83d3a1f6f7f122ca624b3678f56a177
jurgen1507 <jbraan01@gmail.com>
2023-06-21 14:47:11 +0200

    Switch cases added

--- RPLidarA1ArduinoMega.ino
+++ RPLidarA1ArduinoMega.ino
@@ -11,68 +11,33 @@
 #define RPLIDAR_MOTOR 10
 
 const int base_speed = 1000;
-
-// Callback methods prototypes
-// void Lidar();
-// void Motor();
-// void Gyro();
-// void setInput();
+float last_error = 0;
 
 MPU6050 mpu(Wire);
-RPLidar lidar;
 AccelStepper leftMotor(AccelStepper::DRIVER, LEFT_STEP_PIN, LEFT_DIR_PIN);
 AccelStepper rightMotor(AccelStepper::DRIVER, RIGHT_STEP_PIN, RIGHT_DIR_PIN);
 
-float total_angle = 0;
-float input= 0;
-bool is_rotating = false;
-float left;
-float right;
 bool go = true;
 
 
-
-
-// String datastring;
-//     if (data != '&'){
-//       datastring += data;
-//     }
-//     else(){
-//       data_array.append(datastring);
-//       datastring = "";
-//     }
-
 float getAngle(){
-  mpu.update();
   return mpu.getAngleZ();
 }
 
-float checkSpeed(int target_angle){
-  mpu.update();
-  float error = target_angle - getAngle();
-  float speed = abs(error * 0.64);
-  speed = constrain(speed*speed, 0, 255);
-  return speed;
-}
-
 void setMotorSpeed(float leftWheel, float rightWheel){
-  left = leftWheel;
-  right = rightWheel;
-  Motor();
+  leftMotor.setSpeed(1000 - leftWheel);
+  rightMotor.setSpeed((1000 - rightWheel)*-1);
 }
 
-float last_error = 0;
 void rotateTo(int target_angle){
   float angle = getAngle();
   if (abs(target_angle - angle) > 1 || abs(last_error - (target_angle - angle)) > 0.5) {
-    input = 0;
     last_error = target_angle - angle;
-    // float speed = checkSpeed(target_angle);
     if (angle < target_angle) {
-      setMotorSpeed(2000, 0);
+      setMotorSpeed(1000, 0);
     } 
     else {
-      setMotorSpeed(0, 2000);
+      setMotorSpeed(0, 1000);
     }
   } 
   else {
@@ -80,16 +45,7 @@ void rotateTo(int target_angle){
   }
 }
 
-void Motor() {
-  // leftMotor.setSpeed(1000 - left*3.9);
-  // rightMotor.setSpeed((1000 - right*3.9)*-1);
-  leftMotor.setSpeed((1000 - left));
-  rightMotor.setSpeed((1000 - right)*-1);
-}
-
-
 void Gyro(){
-  //rotateTo(0);
   if (go){    
     if (getAngle() != 0){
       rotateTo(0);
@@ -97,20 +53,11 @@ void Gyro(){
   }
 }
 
-void setInput(){
-  // Serial.write(90);
-  input = 90;
-  Serial.print("done");
-}
 void setup () {
-  // Wire.begin(8);
-  // Wire.onReceive(receiveEvent);
   Serial.begin(9600);
   mpu.begin();
   delay(1000);
   mpu.calcOffsets();
-  // lidar.begin(Serial1);
-  // pinMode(RPLIDAR_MOTOR, OUTPUT); 
   leftMotor.setAcceleration(2000);
   rightMotor.setAcceleration(2000);
   leftMotor.setMaxSpeed(base_speed);
@@ -118,37 +65,87 @@ void setup () {
 }
 
 unsigned long previous_millis = 0;
+enum class robotState {DRIVE, STOP, BASE_IN, BASE_OUT};
+robotState currentState = robotState::DRIVE;
 
-
-// void receiveEvent(int howMany) {
-//   while (Wire.available()) {
-//     char c = Wire.read();
-//     if(c == '1') {
-//       Serial.println("Object Detected");
-//       leftMotor.setSpeed(0);
-//       rightMotor.setSpeed(0);
-//       delay(2000);
-//     } else {
-//       leftMotor.setSpeed(255);
-//       rightMotor.setSpeed(255);
-//     }
-//   }
-// }
-
+int counter = 0;
+int new_speed = 0;
+int speed = 0;
 
 void loop () {
-  // unsigned long current_millis = millis();
-  
-  // if (current_millis - previous_millis > 3000)
-  // {
-  //   input = 90;
-  //   previous_millis = current_millis;
-  // }
-  // setMotorSpeed(0,0);
-  // Motor(); 
-// rotateTo(90);   
-  Gyro();
-  // Wire.onReceive(receiveEvent);
+  mpu.update();
+  // Gyro();
   leftMotor.runSpeed();
   rightMotor.runSpeed();
+  
+  if (speed != new_speed){
+    leftMotor.setMaxSpeed(new_speed);
+    rightMotor.setMaxSpeed(new_speed);
+    speed = new_speed;
+  }
+
+  // robotState();
+  // Serial.println(counter);
+  switch (currentState){
+    case robotState::DRIVE:
+      new_speed = 1000;
+      rotateTo(0);
+      counter++;
+      if (counter > 1000){
+        currentState = robotState::STOP;
+        counter = 0;
+      }
+      break;
+
+    case robotState::STOP:
+      leftMotor.stop();
+      rightMotor.stop();
+      setMotorSpeed(1000,1000);
+      counter++;
+      if (counter > 1000){
+        currentState = robotState::BASE_IN;
+        counter = 0;
+      }
+      break;
+    
+
+    case robotState::BASE_IN:
+      new_speed = 300;
+      rotateTo(0);  
+      
+      counter++;
+      if (counter > 1000){
+        currentState = robotState::BASE_OUT;
+        counter = 0;
+      }
+      break;
+
+
+    case robotState::BASE_OUT:
+      new_speed = 300;
+      setMotorSpeed(2000, 2000);
+      counter++;
+      if (counter > 1000){
+        currentState = robotState::DRIVE;
+        counter = 0;
+      }
+      break;
+    // case BASE_OUT:
+
+    
+  }
+  //case1 Rijden
+  
+
+  //case2 Stoppen
+
+  // //case3 Base station
+  // leftMotor.setMaxSpeed(100);
+  // rightMotor.setMaxSpeed(100);
+
+  // //case4 Base station uitrijden
+  // setMotorSpeed(-1000, -1000);
+
+
+
 }
