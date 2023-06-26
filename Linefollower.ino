551850b2640667b3bfcd6c6152af81a956e3e249
Jordydjong <91699735+Jordydjong@users.noreply.github.com>
2023-06-26 11:31:02 +0200

    base code for line follower
    base code without stepper motor integration

---
+++ Linefollower.ino
@@ -0,0 +1,32 @@
+int Sensor1 = 0;
+int Sensor2 = 0;
+int Sensor3 = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Sensor1, INPUT);
+  pinMode(Sensor2, INPUT);
+  pinMode(Sensor3, INPUT);
+}
+
+void loop(){
+  //Read the Sensor if HIGH (BLACK Line) or LOW (WHITE Line)
+  Sensor1 = digitalRead(8);
+  Sensor2 = digitalRead(9);
+  Sensor3 = digitalRead(10);
+
+
+  //Set conditions for FORWARD, LEFT and RIGHT
+
+  if(Sensor3 == HIGH && Sensor2 == LOW && Sensor1 == LOW){
+    Serial.println("right");
+  }
+
+  else if (Sensor3 == LOW && Sensor2 == LOW && Sensor1 == HIGH){
+    Serial.println("left");
+  }
+
+  else{
+    Serial.println("good");
+  }
+}
