c3d3ea02cdec6e5ceabf0ab86cdcd44b28d1e908
Jordydjong <91699735+Jordydjong@users.noreply.github.com>
2023-07-04 10:32:51 +0200

    final code lidar
    lidar detects object thats closer than 400mm then sends a signal to arduino via serial
    CO-AUTHORED-BY: RobinKret <robin.familiekret2@gmail.com>

--- LidarDataTest.py
+++ LidarDataTest.py
@@ -12,40 +12,58 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
 
 blocked = 0
-ser.write("BOOTED\n")
-while True:
-    # print("hai")
-    if not lidar.open():
+
+arduino_react = False 
+# while arduino_react == False:
+#     ser.write(b"BOOTED\n")
+#     line = ser.readline()
+#     data = line.decode().strip()
+#     if data == "HEARD":
+#         arduino_react = True
+last_dir = "P"
+if not lidar.open():
         print("Cannot open lidar")
         exit(1)
+
+while True:
     t = time.time()
-    lijst=[]
-    for i in range(22):
+    direction = ser.readline().decode('utf-8').rstrip()
+    
+    if(last_dir != direction and direction != ""):
+        print(direction)
+    
+    count = 0
+
+    
+    list=[]
+    t = time.time()
+    for i in range(13):
         measures = lidar.getMeasures()  # Get latest lidar measures
-        if len(measures)>=270:
-            lijst.extend(measures)
-        time.sleep(0.015)
-    lidar.close()
+        if len(measures)>=100:
+            list.extend(measures)
+        time.sleep(0.01)
     distance=[]
     degrees=[]
+    
     img = numpy.ones((480, 640))
-    for i in lijst:
+    for i in list:
         x=str(i).split(":")    
         degrees.append(float(x[0]))
         distance.append(float(x[1].split("mm")[0]))
-    
+
     count = 0
     count2 = 0 
     median = []
+    # t = time.time()
     minimum = []
+     
     for j in range(len(degrees)):
         if degrees[j] < 30 or degrees[j] > 330:
             count = count + 1
             if distance[j] != 0:
                 median.append(distance[j])
-            # if distance[j] < 400:
             if len(median) != 0:
-                if count == 15:
+                if count == 5:
                     minimum.append(statistics.median(median))
                     count2 = count2 + 1
                     count = 0
@@ -53,10 +71,14 @@ while True:
                     if count2 == 5:
                         if blocked == 1 and min(minimum) > 400:
                             blocked = 0
-                            print(degrees)
+
+                            print("rijden")
                             ser.write(b"0\n")
                             count2 = 0
                         elif min(minimum) < 400 and blocked == 0:
                             blocked = 1
+                            print("plz stop :(")
                             ser.write(b"1\n")
                             count2 = 0
+# print(t-time.time())
+
